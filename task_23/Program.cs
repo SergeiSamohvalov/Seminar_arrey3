e0a56d231b2d0f1eddd3fba76564ae8404b1c2cb
sergei sam <ssi1970@mail.ru>
2022-07-03 01:24:04 +0700

    Задача 23

---
+++ task_23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу кубов чисел от 1 до N.
+namespace Array
+{
+class Program
+{
+public static void Main(string[] args)
+{
+Console.Write("Введите целое число: ");
+Console.WriteLine();
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 1; i <= n; i++)
+{
+   PrintCube(i);
+}
+
+void PrintCube(int x)
+{
+    //  int value = n;
+    //  int power = 3;     
+     Console.WriteLine(x * x * x);
+}
+}
+}
+}
